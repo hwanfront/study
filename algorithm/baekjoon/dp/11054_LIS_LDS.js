f78befbdceae1c452e776ab58c174ef542a3ae02
hwanfront <kim015jh@gmail.com>
2023-06-14 23:40:58 +0900

    add

---
+++ algorithm/baekjoon/dp/11054_LIS_LDS.js
@@ -0,0 +1,50 @@
+// https://www.acmicpc.net/problem/11054
+// const fs = require("fs");
+// const input = fs.readFileSync("/dev/stdin").toString().trim();
+const input = `10
+1 2 1`
+const [N, A] = input.split('\n');
+console.log(solution(Number(N), A.split(' ').map(Number)));
+
+function solution (N, numbers) {
+  let result = 0;
+  const rdp = Array(N).fill(null);
+  const ldp = Array(N).fill(null);
+
+  const LIS = (n, dp) => { 
+    if(dp[n] === null) {
+      dp[n] = 1;
+
+      for(let i = n - 1; i >= 0; i--) {
+        if(numbers[i] < numbers[n]) {
+          dp[n] = Math.max(dp[n], LIS(i, dp) + 1);
+        }
+      }
+    }
+    return dp[n];
+  }
+
+  const LDS = (n, dp) => { 
+    if(dp[n] === null) {
+      dp[n] = 1;
+
+      for(let i = n + 1; i < dp.length; i++) {
+        if(numbers[i] < numbers[n]) {
+          dp[n] = Math.max(dp[n], LDS(i, dp) + 1);
+        }
+      }
+    }
+    return dp[n];
+  }
+
+
+  for(let i = 0; i < N; i++) {
+    LIS(i, rdp);
+    LDS(i, ldp);
+  }
+  rdp.forEach((e, i) => {
+    result = Math.max(result, e + ldp[i])
+  })
+
+  return result - 1;
+}
