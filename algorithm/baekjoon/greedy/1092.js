55315a3dc7a37293af53b9c1414c2a5e251471d3
hwanfront <kim015jh@gmail.com>
2023-08-04 23:50:53 +0900

    add

---
+++ algorithm/baekjoon/greedy/1092.js
@@ -0,0 +1,31 @@
+// https://www.acmicpc.net/problem/
+// const fs = require("fs");
+// const input = fs.readFileSync("/dev/stdin").toString().trim();
+const input = `3
+10 7 4
+9
+9 9 8 7 6 6 5 5 2`;
+const [N, c, M, b] = input.split('\n');
+console.log(solution(+N, c.split(' ').map(Number), +M, b.split(' ').map(Number)));
+
+function solution (N, c, M, b) {
+  c.sort((a, b) => a - b);
+  b.sort((a, b) => a - b);
+  let result = 0;
+
+  if(c[c.length - 1] < b[b.length - 1]) return -1;
+
+  while(b.length > 0) {
+    result++;
+    for(let i = 0; i < N; i++) {
+      for(let j = b.length - 1; j >= 0; j--) {
+        if(c[i] >= b[j]) {
+          b = b.slice(0, j).concat(b.slice(j + 1))
+          break;
+        }
+      }
+    }
+  }
+
+  return result;
+}
