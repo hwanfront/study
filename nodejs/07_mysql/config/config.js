ec9b8f9943f70c37a135c5ed9fe507c155d0fa6f
hwanfront <kim015jh@gmail.com>
2023-08-19 19:47:01 +0900

    init

---
+++ nodejs/07_mysql/config/config.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const env = process.env.NODE_ENV || 'development';
+
+const config = {
+  development: {
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    dialect: "mysql",
+  },
+  test: {
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    dialect: "mysql"
+  },
+  production: {
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    dialect: "mysql"
+  }
+}
+
+module.exports = config[env];
