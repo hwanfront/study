c50b1b4bbdaa3289869467801e0a4107a2034bd7
hwanfront <kim015jh@gmail.com>
2023-08-22 22:21:15 +0900

    update prod setting

--- nodejs/07_mysql/app.js
+++ nodejs/07_mysql/app.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
-const nunjucks = require('nunjucks');
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const cors = require('cors');
+const hpp = require('hpp');
+const helmet = require('helmet');
 
 const { sequelize } = require('./models');
+const router = require('./routes');
+const userRouter = require('./routes/users')
 
 require('dotenv').config();
 
 const app = express();
 app.set('port', process.env.PORT || 3001);
-app.set('view engine', 'html');
-
-nunjucks.configure('views', {
-  express: app,
-  watch: true
-})
-
 sequelize.sync({ force: false })
   .then(() => {
     console.log('DB 연결 성공');
@@ -24,12 +23,43 @@ sequelize.sync({ force: false })
     console.error(err);
   });
 
-app.use(morgan('dev'));
+const prod = process.env.NODE_ENV === "production";
+
+if(prod) {
+  app.enable("trust proxy");
+  app.use(morgan("combined"));
+  app.use(helmet({ contentSecurityPolicy: false }));
+  app.use(hpp());
+} else {
+  app.use(morgan("dev"));
+  app.use(cors({
+    origin: true,
+    credentials: true,
+    webSocket: true,
+  }))
+}
+
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+const sessionOption = {
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+  }
+}
+if(prod) {
+  sessionOption.cookie.secure = true;
+  sessionOption.cookie.proxy = true;
+}
+app.use(session(sessionOption));
 
 // router
+app.use('/', router);
+app.use('/api/user', userRouter);
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
@@ -37,7 +67,7 @@ app.use((req, res, next) => {
   next(error);
 });
 
-app.use((err, req, res, next) => {
+app.use((err, req, res, next) => { // 에러 처리 middleware는 반드시 next를 추가 
   res.locals.message = err.message;
   res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
   res.status(err.status || 500);
